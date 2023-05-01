e4fb4e380e034f256c54fa239536348bc8ca40d0
mlasala45 <mlasala@uncc.edu>
2023-05-01 10:11:25 -0400

    feat: Links to user profiles
    Usernames on posts are now links.

--- static/js/iframe-post.js
+++ static/js/iframe-post.js
@@ -6,6 +6,8 @@ function load_post(post_element, text, username, timePosted) {
     const htmlStr = iframe.innerHTML
     .replace("HOOK_POST_TEXT", text.replace("\n","<br>"))
     .replace("HOOK_POST_USERNAME", decodeURI(username) + " @ " + decodeURI(timePosted))
+    .replace("HOOK_USERNAME_LINK", "/users/" + username)
+    .replace("p class=\"gwd-p-p2ca\"", "a class=\"gwd-p-p2ca\"");
     iframe.innerHTML = htmlStr;
     console.log(htmlStr);
     console.log(iframe.innerHTML);
